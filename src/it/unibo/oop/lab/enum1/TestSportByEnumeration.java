2ed30fa2b15f9fd5bf83c2c04283021b55749776
saribricka <saribricka44@gmail.com>
2020-12-14 23:10:37 +0100

    almost enum1

--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab.enum1;
 
+import it.unibo.oop.lab.socialnetwork.User;
+
 /**
  * This is going to act as a test for
  * {@link it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl}
@@ -24,6 +26,27 @@ public final class TestSportByEnumeration {
          * 
          * By now, you know how to do it
          */
-        // TODO
+        final SportSocialNetworkUserImpl<User> saribri = new SportSocialNetworkUserImpl("Sara", "Briccoli", "saribricka", 22);		
+        final SportSocialNetworkUserImpl<User> cichito = new SportSocialNetworkUserImpl("Matteo", "Cicognani", "mattecico", 21);
+        final SportSocialNetworkUserImpl<User> loryhm = new SportSocialNetworkUserImpl("Lorenzo", "Briccoli", "lorybricko", 17);
+        
+        saribri.addSport(Sport.SWIM);
+        saribri.addSport(Sport.ENDURO);
+        
+        System.out.println("Sara likes to swim" + saribri.hasSport(Sport.SWIM));
+        System.out.println("Sara likes to play basketball" + saribri.hasSport(Sport.BASKET));
+        
+        cichito.addSport(Sport.VOLLEY);
+        cichito.addSport(Sport.BIKE);
+        
+        System.out.println("Teo likes to ride a bike" + cichito.hasSport(Sport.BIKE));
+        System.out.println("Teo likes to swim" + cichito.hasSport(Sport.SWIM));     
+        
+        loryhm.addSport(Sport.F1);
+        loryhm.addSport(Sport.MOTOCROSS);
+        
+        System.out.println("Lorenzo likes soccer" + loryhm.hasSport(Sport.SOCCER));
+        System.out.println("Lorenzo likes to watch motocross" + loryhm.hasSport(Sport.MOTOCROSS));
+        
     }
 }
